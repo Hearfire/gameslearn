6d73fc92033b402c1dc28d869f5239e88fc88ded
hf <hf@hf.com>
2023-12-03 20:07:56 +0800

    lab5

--- lab5/Triangle.hpp
+++ lab5/Triangle.hpp
@@ -11,6 +11,26 @@ bool rayTriangleIntersect(const Vector3f& v0, const Vector3f& v1, const Vector3f
     // that's specified bt v0, v1 and v2 intersects with the ray (whose
     // origin is *orig* and direction is *dir*)
     // Also don't forget to update tnear, u and v.
+    Vector3f e1=v1-v0,e2=v2-v0;
+    Vector3f s=orig-v0;
+    Vector3f s1=crossProduct(dir,e2);
+    Vector3f s2=crossProduct(s,e1);
+
+    float s1e1=dotProduct(s1,e1);
+    float t=dotProduct(s2,e2)/s1e1;
+    float b1=dotProduct(s1,s)/s1e1;
+    float b2=dotProduct(s2,dir)/s1e1;
+
+    if(t>=0&&b1>=0&&b2>=0&&(1.f-b1-b2)>=0){
+        tnear=t;
+        u=b1;
+        v=b2;
+        return true;
+    }
+
+
+
+
     return false;
 }
 
